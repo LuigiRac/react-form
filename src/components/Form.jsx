698fac14382b419eb12ac35ec0ab1d2969b75c22
LuigiRac <lui.rac06@gmail.com>
2024-12-23 10:15:55 +0100

    console.log(userPosts)

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,12 +2,18 @@ import { useState } from "react";
 
 function Form() {
     const [userPosts, setUserPosts] = useState('Inserisci nuovo post')
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log(userPosts)
+    }
     return (
 
         <section className="input-group my-4">
-            <input type="text" className="form-control" value={userPosts} onChange={(e) => setUserPosts(e.target.value)} />
-            <button className="btn btn-outline-secondary" type="button">Insert</button>
-            <button className="btn btn-outline-secondary" type="button">Delete</button>
+            <form className="d-flex " onSubmit={handleSubmit}>
+                <input type="text" className="form-control" value={userPosts} onChange={(e) => setUserPosts(e.target.value)} />
+                <button className="btn btn-outline-secondary" type="button">Insert</button>
+                <button className="btn btn-outline-secondary" type="button">Delete</button>
+            </form>
         </section>
     )
 }
