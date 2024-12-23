0a15eb9e73a2041fdfc43495bdb9d4adfea84a72
LuigiRac <lui.rac06@gmail.com>
2024-12-23 09:43:15 +0100

    Add "on change"

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,9 +1,11 @@
-function Form() {
+import { useState } from "react";
 
+function Form() {
+    const [userPosts, setUserPosts] = useState('Inserisci nuovo post')
     return (
 
         <section className="input-group my-4">
-            <input type="text" className="form-control" />
+            <input type="text" className="form-control" value={userPosts} onChange={(e) => setUserPosts(e.target.value)} />
             <button className="btn btn-outline-secondary" type="button">Insert</button>
             <button className="btn btn-outline-secondary" type="button">Delete</button>
         </section>
